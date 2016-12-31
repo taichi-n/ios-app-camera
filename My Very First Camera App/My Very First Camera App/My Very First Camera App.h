43b86d21dc02a73a437c2ccd32cbd255e7b47a71
taichi-n <taichinishida@gmail.com>
2016-12-31 16:08:57 +0900

    first commit

---
+++ My Very First Camera App/My Very First Camera App/My Very First Camera App.h
@@ -0,0 +1,19 @@
+//
+//  My Very First Camera App.h
+//  My Very First Camera App
+//
+//  Created by taichi on 2016/12/31.
+//  Copyright © 2016年 taichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for My Very First Camera App.
+FOUNDATION_EXPORT double My_Very_First_Camera_AppVersionNumber;
+
+//! Project version string for My Very First Camera App.
+FOUNDATION_EXPORT const unsigned char My_Very_First_Camera_AppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <My_Very_First_Camera_App/PublicHeader.h>
+
+
